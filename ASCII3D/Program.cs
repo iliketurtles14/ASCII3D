1140806f82d8ca0da0c765f0ddd5aff39b10a1fb
iliketurtles14 <creeperdude81@gmail.com>
2026-08-22 17:23:15 -0400

    8/22/2026

--- ASCII3D/Program.cs
+++ ASCII3D/Program.cs
@@ -1,10 +1,44 @@
 ﻿using System;
+using System.Numerics;
 namespace ASCII3D
 {
     class Program
     {
+        private static Vector2 resolution;
+        private static char[,] screen;
+        private static Vector3 camPos;
+        private static Vector2 lookAngles;
         private static void Main(string[] args)
         {
+            resolution = new Vector2(20, 20);
+            screen = new char[Convert.ToInt32(resolution.X), Convert.ToInt32(resolution.Y)];
+            camPos = new Vector3(0, 0, 0);
+            lookAngles = new Vector2(0, 0);
+
+            RenderLoop();
+        }
+        private static void RenderLoop()
+        {
+            //test stuff
+            Vector3 point = new Vector3(0, 0, 10);
+            
+            while (true)
+            {
+                Console.Clear();
+                string renderStr = "";
+                for(int y = 0; y < resolution.Y; y++)
+                {
+                    for(int x = 0; x < resolution.X; x++)
+                    {
+                        renderStr += "o";
+                        screen[y, x] = 'o';
+                    }
+                    renderStr += "\n";
+                }
+
+                Console.Write(renderStr);
+                Thread.Sleep(16);
+            }
         }
     }
 }
