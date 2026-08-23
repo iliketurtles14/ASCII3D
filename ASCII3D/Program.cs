d82cccf80065e949f5301dd977b5ee5319cb8225
iliketurtles14 <creeperdude81@gmail.com>
2026-08-22 23:41:57 -0400

    8/22/2026 (2)

--- ASCII3D/Program.cs
+++ ASCII3D/Program.cs
@@ -9,7 +9,7 @@ namespace ASCII3D
         private static char[,] screen;
         private static Vector3 camPos;
         private static Vector2 lookAngles;
-        private static int fov;
+        private static int lookDistance;
         private static void Main(string[] args)
         {
             resolution = new Vector2(20, 20);
@@ -45,7 +45,56 @@ namespace ASCII3D
         {
             // .:-=+*#%@
 
+            //make a math function for the line that is cast from the point var in the direction of angles
+            //if hit any shape, get the distance and compare that with the lookDistance.
+            //based on the distance, return a char
 
+            //doing first line (x angle)
+            float run = MathF.Cos(angles[0]);
+            float rise = MathF.Sin(angles[0]);
+            float slope = rise / run;
+
+            List<Vector3> line1Points = new List<Vector3>();
+
+            for(int i = 0; i < lookDistance; i++)
+            {
+                line1Points.Add(new Vector3(i, i * slope, 0));
+            }
+
+            //doing second line (z angle)
+            run = MathF.Cos(angles[1]);
+            rise = MathF.Sin(angles[1]);
+            slope = rise / run;
+
+            List<Vector3> line2Points = new List<Vector3>();
+
+            for(int i = 0; i < lookDistance; i++)
+            {
+                line2Points.Add(new Vector3(i, 0, i * slope));
+            }
+
+            //combine lines
+            List<Vector3> linePoints = new List<Vector3>();
+            for(int i = 0; i < lookDistance; i++)
+            {
+                linePoints.Add(new Vector3(i, line1Points[i].Y, line2Points[i].Z));
+            }
+
+            for(int i = 0; i < lookDistance; i++)
+            {
+                Vector3 realPoint = linePoints[i] + point;
+                foreach(Shape shape in worldShapes)
+                {
+
+                }
+            }
+        }
+        private static List<Vector3> GetShapePoints(Shape shape)
+        {
+            for(int i = 0; i < shape.points.Count; i++)
+            {
+
+            }
         }
     }
 }
